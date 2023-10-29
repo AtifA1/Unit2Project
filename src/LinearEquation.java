feec3fedc9f40fd48e8c8317c0d69ef747ab71c4
Atif Ali_ <aatif1771@gmail.com_>
2023-10-29 14:02:06 -0400

    Finished LinearEquation

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -10,19 +10,19 @@ public class LinearEquation {
         this.x2 = x2;
         this.y2 = y2;
     }
-    public double distance() {  //Calculates the distance between the two points
+    public double distance() {
         double dx = x2 - x1;
         double dy = y2 - y1;
-        return Math.round(Math.sqrt(dx * dx + dy * dy) * 100.0) / 100.0;
-    }
-    public double slope() { //Calculates the slope between the two points
-        return Math.round(((double) (y2 - y1) / (x2 - x1)) * 100.0) / 100.0;
+        return roundedToHundredth(Math.sqrt(dx * dx + dy * dy));
     }
 
-    public double yIntercept() { //Calculates the y-intercept using the first coordinate
-        return Math.round((y1 - (slope() * x1)) * 100.0) / 100.0;
+    public double yIntercept() {
+        return roundedToHundredth(y1 - (slope() * x1));
     }
 
+    public double slope() {
+        return roundedToHundredth(((double) (y2 - y1) / (x2 - x1)));
+    }
     public String equation() { //returns the full equation of the line
         double slope = slope();
         double yIntercept = yIntercept();
@@ -41,7 +41,19 @@ public class LinearEquation {
             return "y = " + slope + "x - " + (-yIntercept);
         }
     }
-
-
+    public String lineInfo() {
+        return "The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n" +
+                "The equation of the line between these points is: " + equation() + "\n" +
+                "The slope of this line is: " + slope() + "\n" +
+                "The y-intercept of this line is: " + yIntercept() + "\n" +
+                "The distance between these points is " + distance();
+    }
+    private double roundedToHundredth(double toRound) { //Rounds to the hundreths place
+        return Math.round(toRound * 100.0) / 100.0;
+    }
+    public String coordinateForX(double x) {
+        double y = roundedToHundredth(slope() * x + yIntercept());
+        return "(" + roundedToHundredth(x) + ", " + y + ")";
+    }
 
 }
